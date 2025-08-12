102e8e6f4c8c60c264e916f6bbe6db24edc3b0bc
lewislim <limyilewis@hotmail.com>
2025-08-12 19:43:37 +0800

    replace tabler with phosphor icons, create paper class

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Button from "@/components/custom/button";
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 import Slider from "rc-slider";
+import { HandPointingIcon } from "@phosphor-icons/react";
 
 export default function Home() {
   const [sliderValue, setSliderValue] = useState<number>(0);
@@ -13,12 +14,13 @@ export default function Home() {
       <Header />
       <div className="head-body pt-[80px] base-padding">
         Main Page
+        <div className="paper w-50 h-50"></div>
         <Button
           text="Click Here"
           icon="/icons/pointer-click.svg"
           size="h-12 w-12"
         />
-        <div className="relative flex flex-col items-center w-10 h-100">
+        <div className="relative flex flex-col items-center w-10 h-100 ">
           <p className="text-center mb-5">{sliderValue}</p>
           <Slider
             min={0}
@@ -28,9 +30,10 @@ export default function Home() {
             onChange={(value) => setSliderValue(value as number)}
             className="flex-1"
           />
-          <img
-            src="/icons/pointer-click.svg"
-            className="absolute bottom-3 left-1/2 -translate-x-1/2 z-10"
+          <HandPointingIcon
+            size={32}
+            color="#f5f5f5"
+            className="icon absolute bottom-3 left-1/2 -translate-x-1/2 z-10"
           />
         </div>
       </div>
